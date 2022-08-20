5e17e68337ce94cb38945997e01907bb3f658823
ZiniaQ <ziniaq@yahoo.com>
2022-08-19 19:41:24 -0500

    added password logic

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 // Assignment code here
-
+var nums = ["1", "2", "3", "4"]
+var uppercase = ["A", "B", "C"]
+var lowercase = ["a", "b"]
+var symbols = ["#", "*", "!"]
+var passwordoptions = []
+function generatePassword() {
+  var finalpassword=[]
+  var length = prompt("how many characters do you want your password to be")
+  console.log(length)
+  if (length < 8) {
+    alert("password length too short")
+    return null
+  }
+  if (length > 128) {
+    alert("password length too long")
+    return null
+  }
+  var hasnumbers = confirm("do you want numbers in your password")
+  var hasuppercase = confirm("do you want uppercase in your password")
+  var haslowercase = confirm("do you want lowercase in your password")
+  var hassymbols = confirm("do you want symbols in your password")
+  if (hasnumbers === true) {
+    passwordoptions = passwordoptions.concat(nums)
+  }
+  if (hasuppercase === true) {
+    passwordoptions = passwordoptions.concat(uppercase)
+  }
+  if (haslowercase === true) {
+    passwordoptions = passwordoptions.concat(lowercase)
+  }
+  if (hassymbols===true){
+    passwordoptions=passwordoptions.concat(symbols)
+     }
+     console.log(passwordoptions)
+     for(var i=0; i<length;i++){
+      finalpassword.push(passwordoptions[])
+    }
+    console.log(finalpassword)
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
